c62db2ab3cbd42db1358a2caa36adb79be77b188
Chris Done <chris.done@artificial.io>
2022-12-12 13:32:39 +0000

    A modest I/O test

--- hell.hs
+++ hell.hs
@@ -15,25 +15,35 @@
 -- wrapped, of course, in an existential.
 
 module Main where
+import Data.Functor.Identity
+import System.Directory
 
 -- Untyped world --------------------------------------------
 data UTerm = UVar String
        | ULam String UType UTerm
        | UApp UTerm UTerm
        | UConBool Bool
+       | UConString String
        | UIf UTerm UTerm UTerm
        | UPure UTerm
        | UPrim Prim
        | UBind UTerm UTerm
 
+data Prim = DoesFileExist
+
+tcPrim :: Prim -> Typed (Term g)
+tcPrim DoesFileExist = Typed (Arr String (Io Bool)) (PrimIO doesFileExist)
+
 data UType =
    UBool
   | UArr UType UType
   | UIo UType
+  | UString
 
 -- Typed world -----------------------------------------------
 data Ty t where
   Bool :: Ty Bool
+  String :: Ty String
   Arr  :: Ty a -> Ty b -> Ty (a -> b)
   Io :: Ty a -> Ty (IO a)
 
@@ -42,9 +52,11 @@ data Term g t where
   Lam :: Ty a -> Term (g,a) b -> Term g (a->b)
   App :: Term g (s -> t) -> Term g s -> Term g t
   ConBool :: Bool -> Term g Bool
+  ConString :: String -> Term g String
   If :: Term g Bool -> Term g a -> Term g a -> Term g a
   Pure :: Term g a -> Term g (IO a)
   Bind :: Term g (IO a) -> Term g (a -> IO b) -> Term g (IO b)
+  PrimIO :: (a -> IO b) -> Term g (a -> IO b)
 
 data Var g t where
   ZVar :: Var (h,t) t
@@ -57,6 +69,7 @@ data ExType = forall t. ExType (Ty t)
 
 tcType :: UType -> ExType
 tcType UBool = ExType Bool
+tcType UString = ExType String
 tcType (UIo t1) = case tcType t1 of ExType t1' -> ExType (Io t1')
 tcType (UArr t1 t2) = case tcType t1 of { ExType t1' ->
               case tcType t2 of { ExType t2' ->
@@ -79,6 +92,10 @@ data Equal a b where
 
 cmpTy :: Ty a -> Ty b -> Maybe (Equal a b)
 cmpTy Bool Bool = Just Equal
+cmpTy String String = Just Equal
+cmpTy (Io a1) (Io b1)
+  = do    { Equal <- cmpTy a1 b1
+    ; return Equal }
 cmpTy (Arr a1 a2) (Arr b1 b2)
   = do    { Equal <- cmpTy a1 b1
     ; Equal <- cmpTy a2 b2
@@ -90,6 +107,10 @@ tc (UVar v) env = case lookupVar v env of
             Typed ty v -> Typed ty (Var v)
 tc (UConBool b) env
   = Typed Bool (ConBool b)
+tc (UConString b) env
+  = Typed String (ConString b)
+tc (UPrim prim) env
+  = tcPrim prim
 tc (UPure a) env
   = case tc a env of
      Typed a_ty' a' -> Typed (Io a_ty') (Pure a')
@@ -126,7 +147,10 @@ showType (Io a) = "(IO " ++ showType a ++ ")"
 uNot = ULam "x" UBool (UIf (UVar "x") (UConBool False) (UConBool True))
 
 test :: UTerm
-test = UBind (UPure (UConBool True)) (ULam "x" UBool (UPure (UApp uNot (UVar "x"))))
+test =
+  UBind
+   (UApp (UPrim DoesFileExist) (UConString "heller.hs"))
+   (ULam "x" UBool (UPure (UIf (UVar "x") (UConString "File exists!") (UConString "File does not exist!"))))
 
 check :: UTerm -> TyEnv () -> Typed (Term ())
 check = tc
@@ -134,7 +158,7 @@ check = tc
 main =  (case check test Nil of
             Typed t ex ->
               case t of
-                Io Bool -> do
+                Io String -> do
                   bool <- eval () ex
                   print bool
                 _ -> pure ()
@@ -143,11 +167,13 @@ main =  (case check test Nil of
 eval :: env -> Term env t -> t
 eval env (Var v) = lookp v env
 eval _   (ConBool c) = c
+eval _   (ConString c) = c
 eval env (If cond true false) = if eval env cond then eval env true else eval env false
 eval env (Lam _ e) = \x -> eval (env, x) e
 eval env (App e1 e2) = (eval env e1) (eval env e2)
 eval env (Pure e1) = pure (eval env e1)
 eval env (Bind m f) = eval env m >>= eval env f
+eval _   (PrimIO a) = a
 
 lookp :: Var env t -> env -> t
 lookp ZVar (_,x)        = x
