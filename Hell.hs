aca3c68fe043d3110efeaa6bfe5462788b0dbf63
Chris Done <git@chrisdone.com>
2024-02-05 15:16:44 +0000

    Add Directory.renameFile

--- Hell.hs
+++ Hell.hs
@@ -725,6 +725,7 @@ supportedLits = Map.fromList [
    ("Directory.getCurrentDirectory", lit (fmap Text.pack Dir.getCurrentDirectory)),
    ("Directory.listDirectory", lit (fmap (fmap Text.pack) . Dir.listDirectory . Text.unpack)),
    ("Directory.setCurrentDirectory", lit (Dir.setCurrentDirectory . Text.unpack)),
+   ("Directory.renameFile", lit (\x y -> Dir.renameFile (Text.unpack x) (Text.unpack y))),
    -- Process
    ("Process.proc", lit $ \n xs -> proc (Text.unpack n) (map Text.unpack xs)),
    ("Process.setEnv", lit $ Process.setEnv @() @() @() . map (bimap Text.unpack Text.unpack)),
