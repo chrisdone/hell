34d50eeb0c28b8e25bb1ab0d324d1ea93228295b
Chris Done <github@chrisdone.com>
2024-12-29 13:15:46 +0000

    Add Map.keys/Map.elems (#66)

--- src/Hell.hs
+++ src/Hell.hs
@@ -1512,6 +1512,8 @@ polyLits =
                  "Map.unionWith" Map.unionWith :: forall k a. (Ord k) => (a -> a -> a) -> Map k a -> Map k a -> Map k a
                  "Map.map" Map.map :: forall a b k. (a -> b) -> Map k a -> Map k b
                  "Map.toList" Map.toList :: forall k a. Map k a -> [(k, a)]
+                 "Map.keys" Map.keys :: forall k a. Map k a -> [k]
+                 "Map.elems" Map.elems :: forall k a. Map k a -> [a]
                  -- Maybe
                  "Maybe.maybe" Maybe.maybe :: forall a b. b -> (a -> b) -> Maybe a -> b
                  "Maybe.Nothing" Maybe.Nothing :: forall a. Maybe a
