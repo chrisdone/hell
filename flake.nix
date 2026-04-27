231871cf5e8160660cb406bbf9e3867aaacef721
Your Name <you@example.com>
2026-04-27 11:12:23 +0100

    Basic flake.nix+flake.lock - what works:
    * managing dependencies works and cabal build
    * nix build .#static-arm64 -- this worked. took a while to build GHC,
      but worked.
    * amd64 seemed like it would work but was burning my CPU off, so will
      just run it without emulation on my amd64 machine.

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "Haskell app with cross-compilation";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachSystem [ "aarch64-linux" ] (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        pkgsStaticArm64 = import nixpkgs {
+          localSystem  = system;
+          crossSystem  = {
+            config = "aarch64-unknown-linux-musl";
+            isStatic = true;
+          };
+        };
+        pkgsStaticAmd64 = import nixpkgs {
+          localSystem  = system;
+          crossSystem  = {
+            config = "x86_64-unknown-linux-musl";
+            isStatic = true;
+          };
+        };
+
+        mkStaticApp = hp: hp.haskellPackages.callCabal2nix "my-app" ./. {};
+
+        app = pkgs.haskellPackages.callCabal2nix "my-app" ./. {};
+      in {
+        devShells.default = pkgs.haskellPackages.shellFor {
+          name = "my-app-dev";
+          packages = _: [ app ];
+          withHoogle = true;
+          buildInputs = with pkgs.haskellPackages; [
+            cabal-install
+            haskell-language-server
+            fourmolu
+            hlint
+            pkgs.zlib
+          ];
+        };
+        packages = {
+          default = app;
+          static-arm64 = mkStaticApp pkgsStaticArm64;
+          static-amd64 = mkStaticApp pkgsStaticAmd64;
+        };
+      }
+    );
+}
